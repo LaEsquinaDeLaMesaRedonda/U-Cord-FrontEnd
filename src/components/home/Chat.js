6a29f5a6638dd0f0442648d9b030e37388f9d17f
Ersocaut <leonardo.soy.yo@hotmail.com>
2021-11-07 16:54:10 -0500

    Confirmación para salir del chat, CSS buttons

--- src/components/home/Chat.js
+++ src/components/home/Chat.js
@@ -2,6 +2,7 @@ import { ChatEngine, ChatEngineContext } from 'react-chat-engine';
 import { UserContext } from '../../context/UserContext';
 import React, { useContext } from 'react';
 import { chatEngineApiClient } from '../../services/chatEngineApiClient';
+import "../../css/Chat.css";
 
 const Chat = () => {
 
@@ -14,26 +15,36 @@ const Chat = () => {
     }
 
     const salirChat = () => {
-        chatEngineApiClient.darDeBaja(user, activeChat);
+        if (window.confirm("¿Estas seguro de querer abandonar esta sala de chat?")) chatEngineApiClient.darDeBaja(user, activeChat);
     };
 
     return (
         <ChatEngine
-            height = "80vh"
+            //Chat functionality options
             projectID = "f2835e2c-343d-4ab3-9944-7e92dc3c6e98"
             userName = {user.correo}
             userSecret = {user.contraseña}
+            offset = {-5}
+
+            //Chat custom UI options
+            height = "88.3vh"
             renderChatSettings={(chatAppState) => 
-            <div>
-                <button onClick = {getChats}>
+            <div
+            allign = "right">
+                <button 
+                type = "button"
+                className = "glow-on-hover"
+                onClick = {getChats} >
                     Mostrar chats (consola).
                 </button>
-                <button onClick = {salirChat} >
+                <button 
+                type = "button"
+                className = "glow-on-hover"
+                onClick = {salirChat} >
                     Salir del chat.
                 </button>
             </div>
             }
-            offset = {-5}
         />
     );
 }
